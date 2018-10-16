2d12c23a5ed9baa307419b0d479fc0b0050cef0f
Brad Wertman <brad.wertman@leanmeanz.com>
2018-10-16 11:28:51 -0400

    cleans up unused Total component from Reservations

--- src/scenes/Reservations/Reservations.js
+++ src/scenes/Reservations/Reservations.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Booking from './components/booking/Booking';
 import Cal from './components/calendar/Calendar.js';
-import Total from './components/total/Total.js';
 import Cards from './components/cards/Cards.js';
 import './Reservations.scss';
 
